428600d8ccbe2a3075641a0546d13371364f0233
杨振宇 <andy@yang.local>
2017-05-05 14:51:26 -0400

    Added CoffeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-var gulp = require("gulp");
-var gutil = require("gulp-util");
+var gulp = require("gulp"),
+    gutil = require("gulp-util"),
+    coffee = require("gulp-coffee");
 
+// // test gulp
+// gulp.task("log", function() {
+//     gutil.log("Workflows are awesome");
+// });
 
-gulp.task("log", function() {
-
-    gutil.log("Workflows are awesome");
-
+var coffeeSrc = ['components/coffee/tagline.coffee'];
+gulp.task('coffee', function() {
+    gulp.src(coffeeSrc)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
 
